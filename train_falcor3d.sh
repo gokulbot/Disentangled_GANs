c0f194890e6eea88e67e8eae2e4aacecfa893669
Anonymous <>
2019-09-25 09:45:26 -0500

    clean up

--- train_falcor3d.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-python main.py --dataset falcor3d_5 --img_size 512 --num_channels 3 --gpu_num 8 --progressive True --phase train \
---labels_fine 0,1,2,3 --labels_coarse 4,5,6 --inp_res 4 --input_layer_type const --D_mode together \
---cond_weight 1.0 --cond_type L2 --use_z True --use_noise False --use_instance_norm True --resume_snapshot -1 \
---batch_size_test 4 --iteration 1280 --max_iteration 25600 --labels_keep_rate 1.0 --style_res 32 --seed 1 \
---FactorVAE False --FID True --MIG True --L2 True --use_style_mod True
